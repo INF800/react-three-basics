9117a54a6cbb1e96d0594e7cfed06d6a355a72b6
Asapanna Rakesh <45640029+INF800@users.noreply.github.com>
2022-06-03 13:45:08 +0530

    initial change

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello INF800</h1>
+      <h2>From codesandbox.io!</h2>
     </div>
   );
 }
